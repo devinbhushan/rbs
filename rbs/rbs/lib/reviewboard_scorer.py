12b468d25739090eab56d02243c97ccbf1fa7df2
Devin Bhushan <devinbhushan@gmail.com>
2013-07-13 03:49:27 -0700

    adding preliminary reviewboard_scorer stuff

--- rbs/rbs/lib/reviewboard_scorer.py
+++ rbs/rbs/lib/reviewboard_scorer.py
@@ -1,12 +1,31 @@
-class ReviewBoardScorer(object):
-    def __init__(self):
+from scorer import Scorer
+from rbs.lib.util import url_to_json
+
+
+class ReviewBoardScorer(Scorer):
+    """Implements Scorer class with ReviewBoard specific scoring heuristics"""
+
+    def __init__(self, username):
+        self.username = username
         pass
 
-    def get_data(self):
+    def _get_data(self):
+        """Gets data from ReviewBoard REST API and gets code that user shipit'ed
+        as well as code he submitted for review"""
+        url_to_json("http://localhost:8080/api/search/?q=%s" % self.username)
         raise NotImplementedError("Should have implemented this")
 
-    def score_positive(self):
+    def _score_positive(self):
         raise NotImplementedError("Should have implemented this")
 
-    def score_negative(self):
+    def _score_negative(self):
         raise NotImplementedError("Should have implemented this")
+
+    def evaluate():
+        """Evaluates score calculations for given user"""
+        self._get_data()
+        self._score_positive()
+        self._score_negative()
+
+rbs = ReviewBoardScorer()
+rbs.get_data()
--- rbs/rbs/lib/scorer.py
+++ rbs/rbs/lib/scorer.py
@@ -1,12 +1,19 @@
 class Scorer(object):
-    def __init__(self):
+    """Abstract class that outlines Scorer functionality, allows for plugins"""
+
+    def __init__(self, username):
+        self.username = username
         pass
 
     def get_data(self):
+        """Gets JSON corresponding to user and stores internal
+        variables needed for calculations"""
         raise NotImplementedError("Should have implemented this")
 
     def score_positive(self):
+        """Calculates positive score for user"""
         raise NotImplementedError("Should have implemented this")
 
     def score_negative(self):
+        """Calculates negative score for user"""
         raise NotImplementedError("Should have implemented this")
