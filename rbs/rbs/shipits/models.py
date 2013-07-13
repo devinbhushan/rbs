3b23e73fa6b2663200c8e49c50cc9f1f856cf05f
Devin Bhushan <devinbhushan@gmail.com>
2013-07-12 21:05:23 -0700

    added models

--- rbs/rbs/shipits/models.py
+++ rbs/rbs/shipits/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=250, primary_key=True)
+    score = models.IntegerField()
+
+    class Meta:
+        app_label = "rbs"
+
+    def __unicode__(self):
+        return self.name
