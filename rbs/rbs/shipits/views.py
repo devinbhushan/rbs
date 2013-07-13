a4755619f7899e6bc07bd090723756789b3c301d
Eric Mills <ericmills2@gmail.com>
2013-07-13 03:47:59 -0700

    big change

--- rbs/rbs/shipits/views.py
+++ rbs/rbs/shipits/views.py
@@ -1,4 +1,5 @@
 from rbs.lib.util import url_to_json
+import json
 
 from django.http import HttpResponse, Http404
 from django.shortcuts import render_to_response
@@ -11,7 +12,7 @@ def index(request):
 		user = request.POST['user']
 		print "username lookup: ", user
 		req = url_to_json("http://10.16.20.100:8080/api/search?username="+user)
-		context = {'resp': req}
+		context = { 'resp': json.dumps(req) }
 	else:
 		print "get"
 	return render_to_response("shipits/search.html", context, context_instance=RequestContext(request))
