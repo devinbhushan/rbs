5fe29b9d7762d8fd5f467621edfec22372cdb6b0
Eric Mills <ericmills2@gmail.com>
2013-07-12 23:41:45 -0700

    awesomeness form user stuff

--- rbs/rbs/shipits/views.py
+++ rbs/rbs/shipits/views.py
@@ -1,7 +1,23 @@
-from django.http import HttpResponse
+import urllib2
+import json
+
+from django.http import HttpResponse, Http404
 from django.shortcuts import render_to_response
+from django.views.generic import View
+from django.template import RequestContext
 
 def index(request):
-    return render_to_response("shipits/search.html")
+	context = {}
+	if request.method == "POST":
+		user = request.POST['user']
+		print "username lookup: ", user
+		req = urllib2.Request("http://10.16.20.100:8080/api/search?username="+user)
+		opener = urllib2.build_opener()
+		f = opener.open(req)
+		data = json.loads(f.read())
+		print data
+	else:
+		print "get"
+	return render_to_response("shipits/search.html", context, context_instance=RequestContext(request))
 
 
